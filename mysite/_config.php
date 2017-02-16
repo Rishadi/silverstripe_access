8e2e144cfbea827c069c39beedb386abc19c5a2c
Rishadi Dahanayake <rishadi2000@gmail.com>
2017-02-16 22:47:12 +1300

    Initial Commit

---
+++ mysite/_config.php
@@ -0,0 +1,17 @@
+<?php
+
+global $project;
+$project = 'mysite';
+
+global $databaseConfig;
+$databaseConfig = array(
+	"type" => 'MySQLDatabase',
+	"server" => 'localhost',
+	"username" => 'root',
+	"password" => '1q2w3e4r',
+	"database" => 'access',
+	"path" => '',
+);
+
+// Set the site locale
+i18n::set_locale('en_US');
